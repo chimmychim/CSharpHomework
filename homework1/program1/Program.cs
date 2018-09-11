48ea744698cbcbe5e17d380963e5af7bc0a1138c
Chimmychim <dadadadakc@163.com>
2018-09-11 13:10:57 +0800

    first commit

---
+++ homework1/program1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "";
+            string b = "";
+            int c = 0;
+            int d = 0;
+            Console.Write("输入一个数: ");
+            a = Console.ReadLine();
+            c = Int32.Parse(a);
+            Console.Write("再输入一个数: ");
+            b = Console.ReadLine();
+            d = Int32.Parse(b);
+            Console.WriteLine("两个数的乘积为: " + c * d);
+        }
+    }
+}
