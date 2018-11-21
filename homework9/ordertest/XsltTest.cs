faee634367f53c65d332ed2832b98a114e07fe47
chimmychim <43114165+chimmychim@users.noreply.github.com>
2018-11-21 20:18:46 +0800

    Add files via upload

---
+++ homework9/ordertest/XsltTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Xml;
+using System.Xml.XPath;
+using System.Xml.Xsl;
+class Test
+{
+    static void Main2()
+    {
+        try
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(@".\ordersTarget.xml");
+
+            XPathNavigator nav = doc.CreateNavigator();
+            nav.MoveToRoot();
+
+            XslTransform xt = new XslTransform();
+            xt.Load(@".\ordersTarget.xslt");
+
+            XmlTextWriter writer = new XmlTextWriter(Console.Out);
+
+            xt.Transform(nav, null, writer);
+        }
+        catch (XmlException e)
+        {
+            Console.WriteLine("XML Exception:" + e.ToString());
+        }
+        catch (XsltException e)
+        {
+            Console.WriteLine("XSLT Exception:" + e.ToString());
+        }
+
+    }
+
+}
